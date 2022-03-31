e011c16ce1293f8d886447fd53cc7f262e494399
Luis Talavera <luisfertalavera15@gmail.com>
2022-03-31 12:06:49 -0600

    Construir ListView separated

---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ListView2Screen extends StatelessWidget {
+  final options = const [
+    'Megaman',
+    'Metal Gear',
+    'Super Smash Bros',
+    'Final Fantasy'
+  ];
+
+  const ListView2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ListView 2')),
+      body: ListView.separated(
+        itemCount: options.length,
+        itemBuilder: (context, index) => ListTile(
+          title: Text(options[index]),
+          trailing: const Icon(Icons.arrow_right_outlined),
+        ),
+        separatorBuilder: (_, __) => const Divider(),
+      ),
+    );
+  }
+}
