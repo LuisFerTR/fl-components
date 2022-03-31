deb32d12b9b02d65e56cbcbc8a27708ee2b65348
Luis Talavera <luisfertalavera15@gmail.com>
2022-03-31 11:37:45 -0600

    Estado inicial

---
+++ lib/screens/listview1_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ListView1Screen extends StatelessWidget {
+  final options = const [
+    'Megaman',
+    'Metal Gear',
+    'Super Smash Bros',
+    'Final Fantasy'
+  ];
+
+  const ListView1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ListView 1')),
+      body: ListView(children: [
+        ...options
+            .map((e) => ListTile(
+                  title: Text(e),
+                  trailing: const Icon(Icons.arrow_right_outlined),
+                ))
+            .toList()
+      ]),
+    );
+  }
+}
