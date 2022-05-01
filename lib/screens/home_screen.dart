22ddda8c80adfdd17acddb755ec60a60eb83f183
Luis F. Talavera R <luisfertalavera15@gmail.com>
2022-05-01 01:31:00 +0000

    Generar rutas de manera dinámica

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,29 +1,31 @@
+import 'package:fl_components/models/menu_option.dart';
+import 'package:fl_components/router/app_routes.dart';
 import 'package:flutter/material.dart';
-import 'package:fl_components/screens/screens.dart'
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-	  appBar: AppBar(
-		  title: const Text('Componentes en Flutter'),
-		  elevation: 0,
-	  ), // AppBar
+    final List<MenuOption> menuOptions = AppRoutes.menuOptions;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Componentes en Flutter'),
+        elevation: 0,
+      ), // AppBar
       body: ListView.separated(
-		  itemBuilder: (context, index) => ListTile(
-			  leading: Icon(Icons.access_time_outlined),
-			  title: Text('Nombre de ruta'),
-			  onTap: () {
-				//final route = MaterialPageRoute(builder: (context) => const ListView1Screen());
-				//Navigator.pushReplacement(context, route);
-				Navigator.pushNamed(context, 'card');
-			  },
-		  ),
-		  separatorBuilder: ( _, __) => const Divider(),
-		  itemCount: 10
-	  ),
+        itemBuilder: (context, i) => ListTile(
+          leading: Icon(menuOptions[i].icon),
+          title: Text(menuOptions[i].name),
+          onTap: () {
+            //final route = MaterialPageRoute(builder: (context) => const ListView1Screen());
+            //Navigator.pushReplacement(context, route);
+            Navigator.pushNamed(context, menuOptions[i].route);
+          },
+        ),
+        separatorBuilder: (_, __) => const Divider(),
+        itemCount: menuOptions.length,
       ),
     );
   }
